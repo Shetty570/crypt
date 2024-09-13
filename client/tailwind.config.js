506e14a610c1f5dca541404fc3be3e8be1f98eba
Suhas Shetty <suhasshetty@Suhass-MacBook-Air.local>
2024-09-12 23:43:34 -0400

    welcome and loader

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,31 @@
+import forms from "@tailwindcss/forms";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // replaced purge with content
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        mf: "990px",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            transform: "translateX(120%)", // simplified the transform property
+          },
+          "100%": {
+            transform: "translateX(0%)", // simplified the transform property
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+      },
+    },
   },
-  plugins: [],
+  plugins: [forms],
 };
